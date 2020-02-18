a2c723dd7c1eb135b4ed21f3f559c5916dce8d2b
TrungKien <kienphantrung@hongphuc.net>
2020-02-18 10:30:58 +0700

    Demo

---
+++ ESP8266_RTOS_SDK/examples/ota_demo/ota/.output/eagle/debug/obj/upgrade_check_api.d
@@ -0,0 +1,32 @@
+.output/eagle/debug/obj/upgrade_check_api.o .output/eagle/debug/obj/upgrade_check_api.d : upgrade_check_api.c \
+ /opt/xtensa-lx106-elf/lib/gcc/xtensa-lx106-elf/4.8.2/include/stdlib.h \
+ /opt/xtensa-lx106-elf/lib/gcc/xtensa-lx106-elf/4.8.2/include/_ansi.h \
+ /opt/xtensa-lx106-elf/lib/gcc/xtensa-lx106-elf/4.8.2/include/newlib.h \
+ /opt/xtensa-lx106-elf/lib/gcc/xtensa-lx106-elf/4.8.2/include/sys/config.h \
+ /opt/xtensa-lx106-elf/lib/gcc/xtensa-lx106-elf/4.8.2/include/machine/ieeefp.h \
+ /opt/xtensa-lx106-elf/lib/gcc/xtensa-lx106-elf/4.8.2/include/stddef.h \
+ /opt/xtensa-lx106-elf/lib/gcc/xtensa-lx106-elf/4.8.2/include/sys/reent.h \
+ /opt/xtensa-lx106-elf/lib/gcc/xtensa-lx106-elf/4.8.2/include/_ansi.h \
+ /opt/xtensa-lx106-elf/lib/gcc/xtensa-lx106-elf/4.8.2/include/sys/_types.h \
+ /opt/xtensa-lx106-elf/lib/gcc/xtensa-lx106-elf/4.8.2/include/sys/lock.h \
+ /opt/xtensa-lx106-elf/lib/gcc/xtensa-lx106-elf/4.8.2/include/machine/stdlib.h \
+ /opt/xtensa-lx106-elf/lib/gcc/xtensa-lx106-elf/4.8.2/include/alloca.h \
+ /opt/xtensa-lx106-elf/lib/gcc/xtensa-lx106-elf/4.8.2/include/stdint.h \
+ /home/esp8266/Share/ESP8266_RTOS_SDK/include/espressif/c_types.h \
+ /opt/xtensa-lx106-elf/lib/gcc/xtensa-lx106-elf/4.8.2/include/stdbool.h \
+ /home/esp8266/Share/ESP8266_RTOS_SDK/include/espressif/spi_flash.h \
+ /home/esp8266/Share/ESP8266_RTOS_SDK/include/lwip/lwip/err.h \
+ /home/esp8266/Share/ESP8266_RTOS_SDK/include/lwip/lwip/opt.h \
+ /home/esp8266/Share/ESP8266_RTOS_SDK/include/lwip/lwipopts.h \
+ /home/esp8266/Share/ESP8266_RTOS_SDK/include/lwip/lwip/debug.h \
+ /home/esp8266/Share/ESP8266_RTOS_SDK/include/lwip/lwip/arch.h \
+ /home/esp8266/Share/ESP8266_RTOS_SDK/include/lwip/arch/cc.h \
+ /opt/xtensa-lx106-elf/lib/gcc/xtensa-lx106-elf/4.8.2/include/stdio.h \
+ /opt/xtensa-lx106-elf/lib/gcc/xtensa-lx106-elf/4.8.2/include/stdarg.h \
+ /opt/xtensa-lx106-elf/lib/gcc/xtensa-lx106-elf/4.8.2/include/sys/types.h \
+ /opt/xtensa-lx106-elf/lib/gcc/xtensa-lx106-elf/4.8.2/include/machine/types.h \
+ /opt/xtensa-lx106-elf/lib/gcc/xtensa-lx106-elf/4.8.2/include/sys/stdio.h \
+ /home/esp8266/Share/ESP8266_RTOS_SDK/include/lwip/lwip/ip_addr.h \
+ /home/esp8266/Share/ESP8266_RTOS_SDK/include/lwip/lwip/def.h \
+ /home/esp8266/Share/ESP8266_RTOS_SDK/include/lwip/ipv4/lwip/ip4_addr.h \
+ /home/esp8266/Share/ESP8266_RTOS_SDK/include/lwip/ipv6/lwip/ip6_addr.h
